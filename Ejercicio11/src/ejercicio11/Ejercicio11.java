084415bfc7e2a2731c0a4abe9018b7246ed7f691
xtianbritos <xtiancuenta1@gmail.com>
2022-06-29 20:53:07 -0300

    Comenzado el ejercicio 11

---
+++ Ejercicio11/src/ejercicio11/Ejercicio11.java
@@ -0,0 +1,16 @@
+
+package ejercicio11;
+
+/*
+Realizar la construcción de un algoritmo que permita de acuerdo a una frase pasada por consola,
+indicar cual es la longitud de esta frase,
+adicionalmente cuantas vocales tiene de “a,e,i,o,u”.
+ */
+
+public class Ejercicio11 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
