93e55c1eb5f34346c4bfd904ec28d1b5c8671712
xtianbritos <xtiancuenta1@gmail.com>
2022-06-30 23:35:56 -0300

    Ejercicio 16 incompleto

--- Ejercicio16/src/ejercicio16/Ejercicio16.java
+++ Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -1,6 +1,9 @@
 
 package ejercicio16;
 
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
 /*
 Haz una clase llamada Persona que siga las siguientes condiciones:
 
@@ -51,9 +54,8 @@ Ahora, crea una clase ejecutable que haga lo siguiente:
 
 *Crea 3 objetos de la clase anterior,
 el primer objeto obtendrá las anteriores variables pedidas por teclado,
-el segundo objeto obtendrá todos los anteriores menos el peso
-y la altura y el último por defecto,
-para este último utiliza los métodos set para darle a los atributos un valor.
+el segundo objeto obtendrá todos los anteriores menos el peso y la altura
+y el último por defecto, para este último utiliza los métodos set para darle a los atributos un valor.
 
 *Para cada objeto, deberá comprobar si está en su peso ideal,
 tiene sobrepeso o por debajo de su peso ideal con un mensaje.
@@ -64,9 +66,59 @@ tiene sobrepeso o por debajo de su peso ideal con un mensaje.
  */
 
 public class Ejercicio16 {
+    
+    public static void mostrarSobrepeso(int res, int n){
+        switch(res) {
+            case -1:
+                JOptionPane.showMessageDialog(null, "La persona "+n+"está en su peso ideal");
+                break;
+            case 0:
+                JOptionPane.showMessageDialog(null, "La persona "+n+" está por dabejo de su peso ideal");
+                break;
+            case 1:
+                JOptionPane.showMessageDialog(null, "La persona "+n+" tiene sobrepeso");
+                break;
+        }
+    }
 
     public static void main(String[] args) {
+        String nombre;
+        int edad;
+        double peso, altura;
+        char sexo;
         
+        nombre = JOptionPane.showInputDialog("Increse un nombre");
+        edad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese una edad"));
+        sexo = (JOptionPane.showInputDialog("Increse un sexo")).toUpperCase().charAt(0);
+        peso = Double.parseDouble(JOptionPane.showInputDialog("Increse un peso"));
+        altura = Double.parseDouble(JOptionPane.showInputDialog("Increse una altura"));
+        
+        Persona p1 = new Persona(nombre, edad, sexo, peso, altura);
+        Persona p2 = new Persona(nombre, edad, sexo);
+        Persona p3 = new Persona();
+        
+        p3.setNombre("Christian");
+        p3.setEdad(28);
+        p3.setAltura(1.80);
+        p3.setPeso(78);
+        p3.setSexo('H');
+        
+        int res = p1.calcularIMC(p1.peso, p1.altura);
+        mostrarSobrepeso(res, 1);
+        
+        res = p2.calcularIMC(p2.peso, p2.altura);
+        mostrarSobrepeso(res, 2);
+
+        res = p3.calcularIMC(p3.peso, p3.altura);
+        mostrarSobrepeso(res, 3);
     }
     
 }
+
+/*
+si la persona está en su peso ideal,
+si esta fórmula devuelve un valor menor que 20, la función devuelve un -1,
+si devuelve un número entre 20 y 25 (incluidos),
+significa que está por debajo de su peso ideal la función devuelve un 0 
+y si devuelve un valor mayor que 25 significa que tiene sobrepeso, la función devuelve un 1.
+*/
---
+++ Ejercicio16/src/ejercicio16/Persona.java
@@ -0,0 +1,118 @@
+
+package ejercicio16;
+
+public class Persona {
+    String nombre="", dni;
+    int edad=0;
+    final char hombre='H';
+    char sexo=hombre;
+    double peso=0, altura=0;
+
+    //Constructores
+    public Persona() {
+        this.dni = generaDNI();
+    }
+    
+    public Persona(String nombre, int edad, char sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = hombre;
+        this.dni = generaDNI();
+    }
+
+    public Persona(String nombre, int edad, char sexo, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = generaDNI();
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    //Métodos
+    public int calcularIMC(double peso, double altura) {
+        
+        if(peso/(altura*altura)<20){
+            return -1;
+        }else if(peso/(altura*altura)>=20 && peso/(altura*altura)< 25){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    
+    public boolean esMayorDeEdad(int edad){
+        if(edad>=18){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public  void comprobarSexo(){
+        if (this.sexo!='H' && this.sexo!='M') {
+            this.sexo = hombre;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return "Nombre: "+this.nombre+
+                " Edad: "+this.edad+
+                " DNI: "+this.dni+
+                " Sexo: "+this.sexo+
+                " Peso: "+this.peso+
+                " Altura: "+this.altura;
+    }
+    
+    public static int generaNumeroRandom(){
+        double numDouble;
+        int numInt;
+        
+        numDouble = 10000000 + Math.random() * 90000000;
+        numInt = (int)numDouble;
+        
+        //Si random da 1 y el número resultante es de 9 cifras le restamos uno
+        if(numInt == 100000000){
+            numInt--;
+        }
+        
+        return numInt;
+    }
+    
+    public static String generaDNI(){
+        String dni;
+        String[] letras = {"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+        int num, i;
+        
+        num = generaNumeroRandom();
+        i = num%23;
+        
+        dni = letras[i]+String.valueOf(num);
+        
+        return dni;
+    }
+    
+    //Métodos set
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+
+}
