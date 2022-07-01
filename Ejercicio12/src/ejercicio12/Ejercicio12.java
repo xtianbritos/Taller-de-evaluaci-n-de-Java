1f4894fe689856deeeaf4d3a4d0d9f6c581495d7
xtianbritos <xtiancuenta1@gmail.com>
2022-07-01 20:25:46 -0300

    Ejercicio 12 mejorado

--- Ejercicio12/src/ejercicio12/Ejercicio12.java
+++ Ejercicio12/src/ejercicio12/Ejercicio12.java
@@ -11,21 +11,40 @@ sino son iguales mostrar sus diferencias.
 public class Ejercicio12 {
 
     public static void main(String[] args) {
-        String palabra1, palabra2, diferencias = "";
+        String palabra1, palabra2, diferencias = "", palabraCorta, palabraLarga;
         
         palabra1 = JOptionPane.showInputDialog("Ingresa una palabra");
         palabra2 = JOptionPane.showInputDialog("Ingresa otra palabra");
         
-        if(palabra1.equals(palabra2)){
+        if(palabra1.equalsIgnoreCase(palabra2)){
             JOptionPane.showMessageDialog(null, "Las dos palabras son iguales");
-        }else{
-            for(int i=0; i<palabra1.length(); i++){
-                if(palabra1.charAt(i) != palabra2.charAt(i)){
-                    diferencias+= String.valueOf(palabra1.charAt(i))+" ";
+            
+        //Si la primera palabra es más corta que la segunda
+        }else if(palabra1.length() < palabra2.length()){
+            palabraCorta = palabra1;
+            palabraLarga = palabra2;
+            
+            for(int i=0; i<palabraLarga.length(); i++){
+                if(i >= palabraCorta.length()){
+                    diferencias+=palabraLarga.charAt(i);
+                }else if(palabraCorta.charAt(i) != palabraLarga.charAt(i)){
+                    diferencias+= String.valueOf(palabraCorta.charAt(i))+" ";
                 }
             }
-            JOptionPane.showMessageDialog(null, "La primera palabra se diferencia de la segunda en las letras: "+diferencias);
+        }else{//Si tienen la misma longitud o la segunda es más larga
+            palabraCorta = palabra2;
+            palabraLarga = palabra1;
+            
+            for(int i=0; i<palabraLarga.length(); i++){
+                if(i >= palabraCorta.length()){
+                    diferencias+=palabraLarga.charAt(i);
+                }else if(palabraCorta.charAt(i) != palabraLarga.charAt(i)){
+                    diferencias+= String.valueOf(palabraCorta.charAt(i))+" ";
+                }
+            }
         }
+            
+        JOptionPane.showMessageDialog(null, "Las palabras se diferencian en las letras: "+diferencias);
     }
     
 }
